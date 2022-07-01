ad9c696eb297082f2f369eda2ac8e7b963e08772
Connorbelez <connor.belez@gmail.com>
2022-07-01 12:11:30 -0400

    drastically improved at large values

--- BunnyBomb.py
+++ BunnyBomb.py
@@ -1,16 +1,42 @@
-def solution(x, y):
+
+def solution(x,y):
     generations = 0
     x,y = int(x),int(y)
     while x > 1 or y > 1:
+
         if x > y:
-            x = x-y
+            print("X:",x,"Y:",y)
+
+
+            truncatedQ = x // y
+            if(x%y) == 0:
+                truncatedQ = truncatedQ-1
+
+            print("Truncated: ",truncatedQ)
+
+            x = x-y*truncatedQ
+
+            # print("X AFTER:",x,"Y after:",y)
+            generations += truncatedQ
+            if truncatedQ == 0:
+                return "impossible"
         elif x < y:
-            y = y-x
+            # print("X:",x,"Y:",y)
+            truncatedQ = y // x
+            if(y%x) == 0:
+                truncatedQ = truncatedQ-1
+            # print("Truncated: ",truncatedQ)
+            y = y-x*truncatedQ
+            # print("X AFTER:",x,"Y after:",y)
+            generations += truncatedQ
+
+            if truncatedQ == 0:
+                return "impossible"
         else:
-            x = x-y
-        generations +=1
+            return "impossible"
         if x < 1 or y < 1:
+            print(x,y)
             return "impossible"
     return str(generations)
 
-solution("4","7")
+print(solution("4","8"))
