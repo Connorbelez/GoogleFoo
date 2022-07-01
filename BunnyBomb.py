b23c362b618e67c8c67682fe1dfd97ed8d442070
Connorbelez <connor.belez@gmail.com>
2022-07-01 12:11:12 -0400

    Level 3 p1 first attempt, failed what is presumably runtime test

--- BunnyBomb.py
+++ BunnyBomb.py
@@ -1,42 +1,16 @@
-
-def solution(x,y):
+def solution(x, y):
     generations = 0
     x,y = int(x),int(y)
     while x > 1 or y > 1:
-
         if x > y:
-            print("X:",x,"Y:",y)
-
-
-            truncatedQ = x // y
-            if(x%y) == 0:
-                truncatedQ = truncatedQ-1
-
-            print("Truncated: ",truncatedQ)
-
-            x = x-y*truncatedQ
-
-            # print("X AFTER:",x,"Y after:",y)
-            generations += truncatedQ
-            if truncatedQ == 0:
-                return "impossible"
+            x = x-y
         elif x < y:
-            # print("X:",x,"Y:",y)
-            truncatedQ = y // x
-            if(y%x) == 0:
-                truncatedQ = truncatedQ-1
-            # print("Truncated: ",truncatedQ)
-            y = y-x*truncatedQ
-            # print("X AFTER:",x,"Y after:",y)
-            generations += truncatedQ
-
-            if truncatedQ == 0:
-                return "impossible"
+            y = y-x
         else:
-            return "impossible"
+            x = x-y
+        generations +=1
         if x < 1 or y < 1:
-            print(x,y)
             return "impossible"
     return str(generations)
 
-print(solution("4","8"))
+solution("4","7")
